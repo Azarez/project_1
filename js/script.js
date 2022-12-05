c83969dedc7c50423e2ebae53bf565e3956645e4
Pavlo <vizzarez@gmail.com>
2022-12-05 15:06:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//const result = confirm('Are u here?');
+
+
+//const answer = prompt ("Вам есть 18?");
+//console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('name:');
+// answers[1] = prompt('age: ');
+// answers[2] = prompt('sex: ');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/4/5/`);
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr++;
+
+console.log(incr, decr);
